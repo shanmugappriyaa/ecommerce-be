e91ea1efb852d9031214fc4d0da4d38f2284171d
shanmugappriyaa <pspri03@gmail.com>
2023-11-19 19:04:00 +0530

    cart model changed

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -365,33 +365,15 @@ const resetPassword = async (req, res) => {
 };
 //add to cart
 const userCart = async(req,res)=>{
-const {cart } = req.body;
+const {productId,quantity,price} = req.body;
 const {_id} = req.user;
 validateMongoDbId(_id)
   try {
-    let products = [];
-    const user = await userModel.findById(_id)
-    const alreadyExistCart = await Cart.findOne({orderby:user._id})
-    if(alreadyExistCart){
-      alreadyExistCart.remove();
-    }
-    for(let i=0;i< cart.length;i++){
-  let object ={};
-  object.product = cart[i]._id;
-  object.count = cart[i].count;
-  object.color = cart[i].color;
-  let getPrice = await Product.findById(cart[i]._id).select("price").exec();
-  object.price = getPrice.price;
-  products.push(object)
-    }
-    let cartTotal = 0;
-    for(let i= 0;i<products.length;i++){
-      cartTotal = cartTotal+ products[i].price*products[i].count
-    }
+
     let newCart = await new Cart({
-      products,
-      cartTotal,
-      orderby:user?._id
+      userId:_id,
+      productId,
+      price,quantity
     }).save()
     res.status(200).send({
       message: "",
